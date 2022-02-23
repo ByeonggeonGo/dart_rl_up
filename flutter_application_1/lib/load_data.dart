a0fd17c17ccc880d610a3cf6a9a970bb71e829cb
SlownSteadi <rhqudrjs2@naver.com>
2022-02-23 17:02:51 +0900

    http연습중

--- flutter_application_1/lib/load_data.dart
+++ flutter_application_1/lib/load_data.dart
@@ -2,22 +2,15 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class DataLoader {
-  final url = Uri.https(
-    'www.googleapis.com',
-    '/books/v1/volumes',
-    {'q': '{http}'},
-  );
 
-  final url2 = Uri.https("api.upbit.com", "/v1/market/all?isDetails=false");
+  final url2 = Uri.https("api.upbit.com", "/v1/market/all");
 
-  get_data() async {
+  get_coin_list() async{
     print('ok');
     final response = await http.get(url2);
     if (response.statusCode == 200) {
       final jsonResponse = convert.jsonDecode(response.body);
       return jsonResponse;
-    } else {
-      return response.statusCode;
-    }
+    } 
   }
 }
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,71 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_application_1/load_data.dart';
+import 'dart:convert' as convert;
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'dt_2_test'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+void main() async {
   DataLoader loader = DataLoader();
-  List<dynamic> lst = [];
+  List lis = await loader.get_coin_list();
+  List liswid = lis.map((e) => Text('$e')).toList();
 
+  print(lis.map((e) => print('$e')));
+  print(liswid);
+
+  runApp(MaterialApp(
+    title: "test",
+    home: ComplicatedImageDemo(),
+  ));
+}
+
+class ComplicatedImageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$lst',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: Text('Complicated image slider demo')),
+      body: Container(
+        child: CarouselSlider(
+          options: CarouselOptions(
+            autoPlay: true,
+            aspectRatio: 2.0,
+            enlargeCenterPage: true,
+          ),
+          items: [Text('1'),Text('1')],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            var temp = loader.get_data();
-            lst.add([9,temp]);
-          });
-          
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
